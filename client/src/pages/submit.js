8472c5497073e8c6329c8ce53b6c4fe0596bd715
Albert Factora <albert.factora@gmail.com>
2017-10-07 07:48:02 -0700

    fixed the sumbit recipe page

--- client/src/pages/submit.js
+++ client/src/pages/submit.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Form from "../components/Form/Form";
-// import API from '../../utils/API';
+import { Container, Row, Col } from "../components/Grid";
+import SearchPic from "../components/SearchPic";
 import "./submit.css";
 
 class NewRecipe extends Component{
@@ -12,13 +13,20 @@ class NewRecipe extends Component{
   }
   render(){
     return(
-      <div className = "row">
-        <div className="col-lg-8" id="heading">
-        <a href="/admin"
+      <div>
+      <SearchPic />
+        <Container>
+           <Row>
+              <Col size="md-12">
+        {/* <div className="col-lg-9" id="heading"> */}
+        {/* <a href="/admin"
             id="admin_redirect">Go back to admin page</a>
-          {/*<h2>Add a new band here!</h2>*/}
+          {<h2>Add a new band here!</h2>} */}
           <Form/>
-        </div>
+        {/* </div> */}
+              </Col>
+           </Row>
+        </Container>
       </div>
     )
   }
